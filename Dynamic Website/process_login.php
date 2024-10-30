3ace2791b16cd00e5632cd721dffbc2353e6317e
Erwan <125487356+ErwanL3Goff@users.noreply.github.com>
2024-10-30 13:23:23 +0100

    Add files via upload

---
+++ Dynamic Website/process_login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'inscription et de connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form-container">
+            <h2>Inscription</h2>
+            <form action="process_registration.php" method="POST">
+                <label for="username">Nom d'utilisateur :</label>
+                <input type="text" id="username" name="username" required>
+                
+                <label for="email">Email :</label>
+                <input type="email" id="email" name="email" required>
+                
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="password" name="password" required>
+                
+                <button type="submit">S'inscrire</button>
+            </form>
+        </div>
+        <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    if ($username == "test" && $password == "1234") {
+        echo "<h2>Connexion réussie</h2>";
+        echo "<p>Bienvenue, $username !</p>";
+    } else {
+        echo "<h2>Échec de la connexion</h2>";
+        echo "<p>Nom d'utilisateur ou mot de passe incorrect.</p>";
+    }
+}
+?>
+
+</html>
+
+
+
+
---
+++ Dynamic Website/process_registration.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire d'inscription et de connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <div class="form-container">
+            <h2>Connexion</h2>
+            <form action="process_login.php" method="POST">
+                <label for="username_login">Nom d'utilisateur :</label>
+                <input type="text" id="username_login" name="username" required>
+                
+                <label for="password_login">Mot de passe :</label>
+                <input type="password" id="password_login" name="password" required>
+                
+                <button type="submit">Se connecter</button>
+            </form>
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                echo "<h2>Inscription réussie</h2>";
+                echo "<p>Nom d'utilisateur : $username</p>";
+                echo "<p>Email : $email</p>";
+            }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
