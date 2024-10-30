db4cb22450602171a4c5fb7d3970f4a1b86790bf
Erwan <125487356+ErwanL3Goff@users.noreply.github.com>
2024-10-30 09:11:07 +0100

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -24,43 +24,7 @@
 
     <?php
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        $nom = $_POST['nom'];
-        $email = $_POST['email'];
-        $mot_de_passe = $_POST['mot_de_passe'];
-
-
-        $servername = "http://localhost:8000/formulaire.php";
-        $username = "Erwan Le Goff";
-        $password = "Cortex91";
-        $dbname = "ma_base";
-
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-
-        if ($conn->connect_error) {
-            die("La connexion a échoué : " . $conn->connect_error);
-        }
-
-
-        $stmt = $conn->prepare("INSERT INTO utilisateurs (nom, email, mot_de_passe) VALUES (?, ?, ?)");
-        $stmt->bind_param("sss", $nom, $email, $mot_de_passe);
-
-
-        if ($stmt->execute()) {
-            echo "<h3>Inscription réussie !</h3>";
-            echo "Nom : " . htmlspecialchars($nom) . "<br>";
-            echo "Email : " . htmlspecialchars($email) . "<br>";
-            echo "Mot de passe : (non affiché pour des raisons de sécurité)<br>";
-        } else {
-            echo "Erreur : " . $stmt->error;
-        }
-
-        $stmt->close();
-        $conn->close();
-    }
-    ?>
+   
 
 </body>
 
